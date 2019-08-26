d1a19ce440be3b221cde550155caacb9640418a4
STHEXA <ti.syu.s@gmail.com>
2019-08-26 18:11:27 +0900

    Create group editing function
    Implemented necessary functions for group editing function.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,9 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
 
   def index
   end
-  
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -17,10 +18,12 @@ class GroupsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
@@ -28,6 +31,9 @@ class GroupsController < ApplicationController
     params.require(:group).permit(:name, { :user_ids => [] })
   end
 
+  def set_group
+    @group = Group.find(params[:id])
+  end
 
 
 
