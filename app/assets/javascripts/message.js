8cb227d86c254d429ddfd4ea11b7a069bb0f237d
STHEXA <ti.syu.s@gmail.com>
2019-09-07 16:34:17 +0900

    Use Ajax to get messages # create to work
    Enabled messages # create to use Ajax when the event fires.
    
    Change the selector to #new_message to fire the event.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,15 @@
 $(function(){
-  $('.form-container').on('submit', function(e){
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
+    var formData = new FormData(this);
+    var href = window.location.href;
+    $.ajax({
+      url: href,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   });
 })
