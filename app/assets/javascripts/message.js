cccef0ceb2ac94ac70a34d9822ed50be706e3387
STHEXA <ti.syu.s@gmail.com>
2019-09-10 18:26:06 +0900

    Create done method and HTML
    The returned JSON is received by the done method, HTML is created and added to the bottom of the message screen.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,61 @@
 $(function(){
+
+  function buildHTML(message){
+    if( (message.content　!= "") && (message.image != null) ){
+      var html = `<div class='message'>
+                    <div class='upper-message'>
+                      <div class='upper-message__user-name'>
+                        ${message.user_name}
+                      </div>
+                      <div class='upper-message__date'>
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class='lower-message'>
+                      <p class='lower-message__content'>
+                        ${message.content}
+                      </p>
+                      <img class="lower-message__image" src="${message.image}" alt="Sample" />
+                    </div>
+                  </div>`
+      return html;
+    }else if((message.content　!= "") && (message.image == null)) {
+      var html = `<div class='message'>
+                    <div class='upper-message'>
+                      <div class='upper-message__user-name'>
+                        ${message.user_name}
+                      </div>
+                      <div class='upper-message__date'>
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class='lower-message'>
+                      <p class='lower-message__content'>
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    }else if((message.content　== "") && (message.image != null)) {
+      var html = `<div class='message'>
+                    <div class='upper-message'>
+                      <div class='upper-message__user-name'>
+                        ${message.user_name}
+                      </div>
+                      <div class='upper-message__date'>
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class='lower-message'>
+                      <p class='lower-message__content'>
+                      <img class="lower-message__image" src="${message.image}" alt="Sample" />
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    }
+  };
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +67,14 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('#message_content').val('')
+    })
+    .fail(function(){
+      alert(erorr);
     })
   });
 })
