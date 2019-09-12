f2c9d517c9615dd7db8582df54594889b29d09b6
STHEXA <ti.syu.s@gmail.com>
2019-09-12 18:13:48 +0900

    Fix controller
    Delete because redirect_to is not required

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def index
     @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
     respond_to do |format|
-      format.html { redirect_to root_path  }
+      format.html
       format.json
     end
   end
