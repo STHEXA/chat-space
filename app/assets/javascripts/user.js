6d720ead5486d9ed1d265cc4840c6d12f1bb3862
STHEXA <ti.syu.s@gmail.com>
2019-09-12 16:45:35 +0900

    Set asynchronous communication
    Created to allow asynchronous communication at the event.
    
    I set it to allow asynchronous communication, and set the routing of url to be user # index.

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,21 @@
 $(function(){
 
-  $('#user-search-field').on('keyup', function(e){
+  $('#edit_group_1').on('keyup', function(e){
     e.preventDefault();
+    var formData = new FormData(this);
+    var href = 'http://localhost:3000/users';
+    
+    $.ajax({
+      url: href,
+      type: 'GET',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      
+    })
+    
   });
-})
+});
