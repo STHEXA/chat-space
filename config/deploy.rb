d03e0017c123602470e390a213b4b74866ccd7f3
STHEXA <ti.syu.s@gmail.com>
2019-09-18 22:42:40 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,13 +3,13 @@
 lock '3.11.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-space''
+set :application, 'chat-space'
 
 set :repo_url, "git@github.com:STHEXA/chat-space.git"
 set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads")
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1'
+set :rbenv_ruby, "2.5.1"
 
 set :ssh_options, auth_methods: ["publickey"],
                   keys: ["~/.ssh/sthexakey.pem"]
@@ -19,9 +19,9 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-after 'deploy:publishing', 'deploy:restart'
+after "deploy:publishing", "deploy:restart"
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:restart'
+    invoke "unicorn:restart"
   end
 end
